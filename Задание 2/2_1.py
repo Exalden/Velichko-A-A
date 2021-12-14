d95405c2bb402bc02901392860f082619bb1eeb2
Exalden <96061058+Exalden@users.noreply.github.com>
2021-12-14 05:26:45 +0300

    Add files via upload

---
+++ Задание 2/2_1.py
@@ -0,0 +1,7 @@
+# -- coding: utf-8 --
+print("Введите числа")
+a = int(input())
+b = int(input())
+c= int(input())
+n = a+b+c
+print(n)
---
+++ Задание 2/2_2.py
@@ -0,0 +1,6 @@
+# -- coding: utf-8 --
+print("Введите катеты")
+a = int(input())
+b = int(input())
+S = 1/2*a*b
+print(S)
