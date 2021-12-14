449f2f57e1627e4c73a090970c812969f1ec5111
Exalden <96061058+Exalden@users.noreply.github.com>
2021-12-14 09:31:15 +0300

    Delete Задание 2 directory

--- Задание 2/2_1.py
+++
@@ -1,7 +0,0 @@
-# -- coding: utf-8 --
-print("Введите числа")
-a = int(input())
-b = int(input())
-c= int(input())
-n = a+b+c
-print(n)
--- Задание 2/2_2.py
+++
@@ -1,6 +0,0 @@
-# -- coding: utf-8 --
-print("Введите катеты")
-a = int(input())
-b = int(input())
-S = 1/2*a*b
-print(S)
